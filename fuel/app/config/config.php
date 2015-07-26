a242f6a97e3e994878dafedf19e63b720fb06b79
Javi Carretero <fjcarretero@ximdex.com>
2015-07-26 23:17:08 +0200

    Configuring instance

--- fuel/app/config/config.php
+++ fuel/app/config/config.php
@@ -256,7 +256,7 @@ return array(
 	/**************************************************************************/
 	/* Always Load                                                            */
 	/**************************************************************************/
-	// 'always_load'  => array(
+	 'always_load'  => array(
 
 		/**
 		 * These packages are loaded on Fuel's startup.
@@ -269,9 +269,9 @@ return array(
 		 *     array('auth'	=> PKGPATH.'auth/')
 		 * );
 		 */
-		// 'packages'  => array(
-		// 	//'orm',
-		// ),
+		 'packages'  => array(
+		 	'orm',
+		 ),
 
 		/**
 		 * These modules are always loaded on Fuel's startup. You can specify them
@@ -307,6 +307,6 @@ return array(
 		 * If you don't want the lang in a group use null as groupname.
 		 */
 		// 'language'  => array(),
-	// ),
+	 ),
 
 );
---
+++ fuel/app/migrations/001_create_usuarios.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Fuel\Migrations;
+
+class Create_usuarios
+{
+	public function up()
+	{
+		\DBUtil::create_table('usuarios', array(
+			'id' => array('constraint' => 11, 'type' => 'int', 'auto_increment' => true, 'unsigned' => true),
+			'nombre' => array('constraint' => 255, 'type' => 'varchar'),
+			'password' => array('constraint' => 255, 'type' => 'varchar'),
+			'email' => array('constraint' => 255, 'type' => 'varchar'),
+			'role' => array('constraint' => 11, 'type' => 'int'),
+			'created_at' => array('constraint' => 11, 'type' => 'int', 'null' => true),
+			'updated_at' => array('constraint' => 11, 'type' => 'int', 'null' => true),
+
+		), array('id'));
+	}
+
+	public function down()
+	{
+		\DBUtil::drop_table('usuarios');
+	}
+}
