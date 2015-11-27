168344c35046eddfc8489ee347769489044f74c9
Javi Carretero <fjcarretero@ximdex.com>
2015-11-27 18:50:03 +0100

    Minor changes

--- fuel/app/views/tipo/ficheros/index.php
+++ fuel/app/views/tipo/ficheros/index.php
@@ -1,4 +1,6 @@
 <h2>Tipos de <span class='muted'>ficheros</span> dados de alta en el sistema</h2>
+<p>Por seguridad hemos eliminado la acción de borrar cualquier tipo de fichero aquí definido pues tendría consecuencias
+    en el listado de ficheros cedidos entre clientes. Si deseas borrar alguno, contacta con el <b>administrador</b>.</p>
 <br/>
 <?php if ($tipo_ficheros): ?>
 <table class="table table-striped">
@@ -19,7 +21,7 @@
 					<div class="btn-group">
 						<?php echo Html::anchor('tipo/ficheros/view/'.$item->id, '<span class="glyphicon glyphicon-eye-open"></span> Detalle', array('class' => 'btn btn-default')); ?>
                         <?php echo Html::anchor('tipo/ficheros/edit/'.$item->id, '<span class="glyphicon glyphicon-pencil"></span> Editar', array('class' => 'btn btn-success')); ?>
-                        <?php echo Html::anchor('tipo/ficheros/delete/'.$item->id, '<span class="glyphicon glyphicon-trash"></span> Borrar tipo', array('class' => 'btn btn-danger', 'onclick' => "return confirm('¿Estás seguro de querer eliminarlo de forma permanente?')")); ?>
+                        <?php /*echo Html::anchor('tipo/ficheros/delete/'.$item->id, '<span class="glyphicon glyphicon-trash"></span> Borrar tipo', array('class' => 'btn btn-danger', 'onclick' => "return confirm('¿Estás seguro de querer eliminarlo de forma permanente?')"));*/ ?>
                     </div>
 				</div>
 
