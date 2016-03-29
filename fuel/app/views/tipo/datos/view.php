dbe182085f3b71e808f7a21f5681fa7df91338c8
Javi Carretero <rentonr11@gmail.com>
2016-03-29 13:22:27 +0200

    Improving interface

--- fuel/app/views/tipo/datos/view.php
+++ fuel/app/views/tipo/datos/view.php
@@ -1,14 +1,21 @@
-<h2>Viewing <span class='muted'>#<?php echo $tipo_dato->id; ?></span></h2>
-
+<h2>Mostrando detalle del <span class='muted'>tipo de dato</span> seleccionado</h2>
+<br/>
 <p>
 	<strong>Nombre:</strong>
 	<?php echo $tipo_dato->nombre; ?></p>
 <p>
-	<strong>Tipo:</strong>
-	<?php echo $tipo_dato->tipo; ?></p>
+	<strong>Tipo de dato:</strong>
+	<?php
+    $tipo_ops = array("Datos de carácter identificativo","Datos de características personales",
+        "Datos de circunstancias sociales","Datos académicos y profesionales","Datos de detalles de empleo",
+        "Datos de información comercial","Datos económico-financieros y de seguros","Datos de transacciones",
+        "Datos especialmente protegidos");
+    echo $tipo_ops[$tipo_dato->tipo]; ?></p>
 <p>
-	<strong>Nivel:</strong>
-	<?php echo $tipo_dato->nivel; ?></p>
-
-<?php echo Html::anchor('tipo/datos/edit/'.$tipo_dato->id, 'Edit'); ?> |
-<?php echo Html::anchor('tipo/datos', 'Back'); ?>
+	<strong>Nivel de seguridad:</strong>
+	<?php
+    $nivel_ops = array("Básico","Medio","Alto");
+    echo $nivel_ops[$tipo_dato->nivel]; ?></p>
+<br/>
+<?php echo Html::anchor('tipo/datos/edit/'.$tipo_dato->id, '<span class="glyphicon glyphicon-pencil"></span> Editar dato',array('class'=>'btn btn-success')); ?>&nbsp;&nbsp;
+<?php echo Html::anchor('tipo/datos', '<span class="glyphicon glyphicon-trash"></span> Volver',array('class'=>'btn btn-danger')); ?>
