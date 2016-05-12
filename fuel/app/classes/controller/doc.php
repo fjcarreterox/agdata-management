1e03a8a9f3a2f8b40668b6ada213a8945cb2b003
Javi Carretero <rentonr11@gmail.com>
2016-05-12 11:10:41 +0200

    Minor change related with the last commit

--- fuel/app/classes/controller/doc.php
+++ fuel/app/classes/controller/doc.php
@@ -144,26 +144,23 @@ class Controller_Doc extends Controller_Template{
         $files = array();
         if($isCPP){
             $data['pres'] = Model_Personal::find('first',array('where'=>array('idcliente'=>$idc,'relacion'=>6)));
-            $rel_aaffs = Model_Rel_Comaaff::find('all',array('where'=>array('idcom'=>$idc)));
-            $reps = array();
-            $i=0;
-            foreach($rel_aaffs as $r){
-                $rep_legal = Model_Personal::find('first',array('where'=>array('idcliente'=>$r->idaaff,'relacion'=>1)));
-                $reps[$i]["nombre"] = $rep_legal->nombre;
-                $reps[$i]["dni"] = $rep_legal->dni;
-                $reps[$i]["nombre_aaff"] = Model_Cliente::find($r->idaaff)->get('nombre');
-                $reps[$i]["dir"] = Model_Cliente::find($r->idaaff)->get('direccion');
-                $reps[$i]["cp"] = Model_Cliente::find($r->idaaff)->get('cpostal');
-                $reps[$i]["loc"] = Model_Cliente::find($r->idaaff)->get('loc');
-                $reps[$i]["prov"] = Model_Cliente::find($r->idaaff)->get('prov');
-                $i++;
-            }
-            $data['reps'] = $reps;
+            $rep_legal = Model_Personal::find('first',array('where'=>array('idcliente'=>$idces,'relacion'=>1)));
+
+            $rep["nombre"] = $rep_legal->nombre;
+            $rep["dni"] = $rep_legal->dni;
+            $rep["nombre_aaff"] = Model_Cliente::find($idces)->get('nombre');
+            $rep["dir"] = Model_Cliente::find($idces)->get('direccion');
+            $rep["cp"] = Model_Cliente::find($idces)->get('cpostal');
+            $rep["loc"] = Model_Cliente::find($idces)->get('loc');
+            $rep["prov"] = Model_Cliente::find($idces)->get('prov');
+
             $files_tmp = Model_Fichero::find('all',array('where'=>array('idcliente'=>$idc)));
             foreach($files_tmp as $f){
                 $files[]["type"] = Model_Tipo_Fichero::find($f->idtipo)->get('tipo');
             }
+
             $data['files'] = $files;
+            $data['rep'] = $rep;
             return View::forge('doc/cesion_cpp',$data)->render();
         }
         else{
