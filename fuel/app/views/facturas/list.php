aad61bc95db13ac85441f7b688e3d0c5c8732a29
Javi Carretero <rentonr11@gmail.com>
2016-08-09 22:17:08 +0200

    Minor change to show the correct format on the invoice number

--- fuel/app/views/facturas/list.php
+++ fuel/app/views/facturas/list.php
@@ -17,13 +17,14 @@
         <td><?php echo Model_Cliente::find(Model_Contrato::find(Model_Servicios_Contratado::find($item->idsc)->get('idcontrato'))->get('idcliente'))->get('nombre'); ?></td>
 		<td><?php
             if(strcmp($item->num_fact,"")==0){echo '<span class="red">N/D</span>';}
-            else{echo $item->num_fact;} ?></td>
+            else{echo "L".str_pad($item->num_fact, 3, 0, STR_PAD_LEFT)."/".$item->anyo_cobro;} ?></td>
 		<td><?php echo $item->estado; ?></td>
 		<td>
 			<div class="btn-toolbar">
 				<div class="btn-group">
-                    <?php echo Html::anchor('facturas/edit/'.$item->id, '<span class="glyphicon glyphicon-pencil"></span> Emitir', array('class' => 'btn btn-success'));?>
-                    <?php echo Html::anchor('doc/factura/'.$item->id, '<span class="glyphicon glyphicon-file"></span> Imprimir', array('class' => 'btn btn-info')); ?>
+                    <?php echo Html::anchor('facturas/issue/'.$item->id, '<span class="glyphicon glyphicon-forward"></span> Emitir', array('class' => 'btn btn-warning'));?>
+                    <?php echo Html::anchor('facturas/edit/'.$item->id, '<span class="glyphicon glyphicon-pencil"></span> Cambiar estado', array('class' => 'btn btn-success'));?>
+                    <?php echo Html::anchor('doc/factura/'.$item->id, '<span class="glyphicon glyphicon-file"></span> Imprimir', array('class' => 'btn btn-info','target'=>'_blank')); ?>
                     <?php /*echo Html::anchor('facturas/delete/'.$item->id, '<i class="icon-trash icon-white"></i> Delete', array('class' => 'btn btn-sm btn-danger', 'onclick' => "return confirm('Are you sure?')"));*/ ?>
                 </div>
 			</div>
