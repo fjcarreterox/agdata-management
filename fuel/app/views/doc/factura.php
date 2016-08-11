7492efaee89e77b2af571b105547628fbe34e5c6
Javi Carretero <rentonr11@gmail.com>
2016-08-11 23:18:47 +0200

    Minor changes on invoices and in the privacy request
    Agreed with the customer

--- fuel/app/views/doc/factura.php
+++ fuel/app/views/doc/factura.php
@@ -10,16 +10,16 @@ class PDFp extends PDF_MC_Table{
     function Header(){
         $this->AddFont('Futura','','ufonts.com_futura-book.php');
         $this->SetTextColor(0, 80, 185);
-        $this->SetFont('Futura','',24);
-        $this->Cell(0,5,'FACTURA',0,0,'R');
+        $this->SetFont('Helvetica','B',24);
+        $this->Cell(0,5,'FACTURA   ',0,0,'R');
         $this->Ln(5);
         $this->SetFont('Futura','',11);
-        $this->Cell(0,15,utf8_decode("Nº L".$this->num_f),0,0,'R');
+        $this->Cell(0,15,utf8_decode("Nº L".$this->num_f).'       ',0,0,'R');
         $this->Ln(10);
         $fecha = explode("-",date("d-m-Y"));
-        $this->Cell(0,10,'Fecha: '.$fecha[0].' de '.getMes($fecha[1]).' de '.$fecha[2],0,1,'R');
+        $this->Cell(0,10,'Fecha: '.$fecha[0].' de '.getMes($fecha[1]).' de '.$fecha[2].'       ',0,1,'R');
         $this->Image('http://gestion.agdata.es/assets/img/logo2.png',20,13,40);
-        $this->Ln(10);
+        $this->Ln(20);
     }
 
     function Footer(){
@@ -46,19 +46,23 @@ $pdf->AddPage();
 $pdf->SetFont('Arial','B',11);
 $pdf->SetWidths(array(110,50));
 $pdf->SetAligns(array('L','L'));
-$pdf->Row(array('NOMBRE: '.$cname,'CIF: '.$cif));
-$pdf->Row(array('DIRECCIÓN: '.html_entity_decode($dir),'C.P: '.$cp));
-$pdf->Row(array('LOCALIDAD: '.html_entity_decode($loc),'PROVINCIA: '.$prov));
-$pdf->Ln(5);
+$pdf->Row(array('NOMBRE:           '.$cname,'CIF:                '.$cif));
+$pdf->SetWidths(array(160));
+$pdf->SetAligns(array('L'));
+$pdf->Row(array('DIRECCIÓN:       '.html_entity_decode($dir)));
+$pdf->SetWidths(array(110,50));
+$pdf->SetAligns(array('L','L'));
+$pdf->Row(array('LOCALIDAD:      '.html_entity_decode($loc),'C.POSTAL:    '.$cp));
+$pdf->Ln(10);
 
 /* estipulaciones */
 $pdf->SetFont('Arial','B',12);
 $pdf->Cell(0,10,strtoupper('servicios:'),0,1,'L');
 $pdf->Ln(5);
-$pdf->Cell(0,10,strtoupper(utf8_decode(html_entity_decode($nombre_serv)).' A.G.DATA LOPD - CUOTA '.$num_cuota.' DE '.$total_fact),0,1,'C');
+$pdf->Cell(0,10,strtoupper(utf8_decode(html_entity_decode($nombre_serv)).' - CUOTA '.$num_cuota.' DE '.$total_fact),0,1,'C');
 
 $pdf->SetFillColor(0, 80, 185);
-$pdf->RoundedRect(15, 142, 180, 1.5, 0.5, 'DF');
+$pdf->RoundedRect(20, 156, 160, 0.7, 0.5, 'DF');
 $pdf->Ln(10);
 /* list of contents */
 $pdf->SetFont('Arial','',10);
@@ -66,34 +70,26 @@ $pdf->MultiCell(0,6,utf8_decode('- ACTUALIZACIÓN DEL DOCUMENTO DE SEGURIDAD DE 
 $pdf->MultiCell(0,6,utf8_decode('- ASESORAMIENTO JURIDICO EN MATERIA DE PROTECCIÓN DE DATOS.'),0,'L');
 $pdf->MultiCell(0,6,utf8_decode('- REDACCIÓN / ADAPTACIÓN DE DOCUMENTOS LEGALES ADICIONALES.'),0,'L');
 $pdf->MultiCell(0,6,utf8_decode('- ADAPTACIÓN A NOVEDADES EN LA NORMATIVA SOBRE PROTECCIÓN DE DATOS.'),0,'L');
-$pdf->MultiCell(0,6,utf8_decode('- CONTACTO DIRECTO CON EL RESPONSABLE DE SEGURIDAD.'),0,'L');
+$pdf->MultiCell(0,6,utf8_decode('- CONTACTO DIRECTO CON EL RESPONSABLE DE SEGURIDAD DESIGNADO.'),0,'L');
 $pdf->MultiCell(0,6,utf8_decode('- AUDITORIA BIANUAL OBLIGATORIA PARA NIVEL MEDIO DE SEGURIDAD.'),0,'L');
 $pdf->Ln(3);
 
-$pdf->SetLeftMargin(120);
+$pdf->SetLeftMargin(100);
 $pdf->SetFont('Arial','B',10);
 $pdf->SetWidths(array(30,35));
 $pdf->SetAligns(array('C','R'));
-$pdf->Row(array("IMPORTE",$importe.' Euros'));
-$pdf->Row(array("I.V.A. (21%)",number_format($importe*0.21,2).' Euros'));
-$pdf->Row(array("TOTAL",number_format($importe*1.21,2).' Euros'));
+$pdf->Row(array("IMPORTE",$importe.'  Euros'));
+$pdf->Row(array("I.V.A. (21%)",number_format($importe*0.21,2).'  Euros'));
+$pdf->Row(array("TOTAL",number_format($importe*1.21,2).'  Euros'));
 $pdf->Ln(5);
 
 $pdf->SetLeftMargin(20);
 $pdf->Ln(2);
 $pdf->SetFont('Arial','',10);
 $pdf->Cell(0,6,utf8_decode('FORMA DE PAGO: '.html_entity_decode($forma)),0,'L');
-
+$pdf->Ln(45);
 $pdf->SetLeftMargin(20);
-$pdf->Image('http://gestion.agdata.es/assets/img/firma.png',120,170,60);
-$pdf->Ln(10);
-
-/*$pdf->SetLeftMargin(20);
-$pdf->Ln(10);
-$pdf->SetFont('Arial','B',10);
-$pdf->MultiCell(0, 6, utf8_decode('Dpto. Administración'),0,'L');
-$pdf->MultiCell(0, 6, utf8_decode('Análisis y Gestión de Datos SL'),0,'L');
-$pdf->Ln(10);*/
+$pdf->Image('http://gestion.agdata.es/assets/img/firma.png',120,217,60);
 
 $pdf->SetTextColor(0, 80, 185);
 $pdf->SetFont('Arial','B',9);
