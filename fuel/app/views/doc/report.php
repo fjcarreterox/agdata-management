9b0a78c0e8cbb07a45d21c62350ec05d26ad3cbd
Javi Carretero <rentonr11@gmail.com>
2017-01-18 07:16:53 +0100

    New report to find invoices

--- fuel/app/views/doc/report.php
+++ fuel/app/views/doc/report.php
@@ -2,7 +2,7 @@
 <p>Se irán añadiendo más informes que faciliten la gestión de los clientes y de su relación con AGDATA.</p>
 <ul>
     <li><a href="../contrato/month_search">Buscar servicios por fecha</a></li>
-    <li>...</li>
+    <li><a href="../facturas/month_search">Buscar facturas en un mes concreto</a></li>
     <li>...</li>
     <li>...</li>
 </ul>
