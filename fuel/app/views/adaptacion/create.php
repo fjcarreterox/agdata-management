f398b918ba2ce5fbda0964a3a56342327c6f8d03
Javi Carretero <fjcarretero.garcia@gmail.com>
2019-02-06 00:09:27 +0100

    Minor change in the return button

--- fuel/app/views/adaptacion/create.php
+++ fuel/app/views/adaptacion/create.php
@@ -3,4 +3,4 @@
 <?php
 $data['idcliente'] = $idcliente;
 echo render('adaptacion/_form',$data); ?>
-<p><?php echo Html::anchor('cliente/view/'.$idcliente, '<span class="glyphicon glyphicon-backward"></span> Volver a la ficha de cliente',array('class'=>'btn btn-danger')); ?></p>
+<p><?php echo Html::anchor('clientes/view/'.$idcliente, '<span class="glyphicon glyphicon-backward"></span> Volver a la ficha de cliente',array('class'=>'btn btn-danger')); ?></p>
