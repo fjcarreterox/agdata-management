4689353916799dc374a42c6285361e968054d825
Javi Carretero <fjcarretero.garcia@gmail.com>
2023-08-09 07:59:21 +0200

    Minor changes

--- fuel/app/views/servicios/create.php
+++ fuel/app/views/servicios/create.php
@@ -1,4 +1,5 @@
 <h2>Crear un nuevo <span class='muted'>servicio</span> en el sistema</h2>
+<h3>(La categoría ya no se está usando activamente en el sistema. Lo importante es el Nombre del servicio)</h3>
 <br/>
 <?php echo render('servicios/_form'); ?>
 <p><?php echo Html::anchor('servicios', '<span class="glyphicon glyphicon-backward"></span>  Volver al listado de servicios',array('class'=>'btn btn-danger')); ?></p>
--- fuel/app/views/servicios/edit.php
+++ fuel/app/views/servicios/edit.php
@@ -1,4 +1,6 @@
 <h2>Editando detalle del <span class='muted'>servicio</span> seleccionado</h2>
+<h3><font color="red"><u>ATENCIÓN</u>: Editar el <strong>nombre del servicio</strong> puede acarrear efectos no deseados en los contratos de los clientes,
+    tanto en su generación en PDF, como en la base de datos.</font></h3>
 <br/>
 <?php echo render('servicios/_form'); ?>
 <p><?php echo Html::anchor('servicios/view/'.$servicio->id, '<span class="glyphicon glyphicon-eye-open"></span> Detalle',array('class'=>'btn btn-default')); ?>&nbsp;
